fe0e172c3d9805a984e8bdb14ab63d88994c02bf
Radu Ciorba <radu@presslabs.com>
2015-12-12 00:22:01 +0200

    switch from httpc to hackney

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROJECT = erltutorial
-DEPS = cowboy jiffy # erlastic_search hackney jsx
+DEPS = cowboy jiffy hackney # erlastic_search jsx
 # dep_erlastic_search = git https://github.com/tsloughter/erlastic_search master
-# dep_hackney = git http://github.com/benoitc/hackney master
+dep_hackney = git http://github.com/benoitc/hackney master
 # dep_jsx = git https://github.com/talentdeficit/jsx master
 include erlang.mk
--- src/hello_handler.erl
+++ src/hello_handler.erl
@@ -22,30 +22,31 @@ prepare_request(Req) ->
     {MethodBinary, _} = cowboy_req:method(Req),
     Method = method_bin_to_atom(MethodBinary),
     {HeadersBinary, _} = cowboy_req:headers(Req),
-    Headers = [{binary:bin_to_list(Header), binary:bin_to_list(Value)} ||
-                  {Header, Value} <- HeadersBinary],
+    %% Headers = [{binary:bin_to_list(Header), binary:bin_to_list(Value)} ||
+    %%               {Header, Value} <- HeadersBinary],
     Host = "http://localhost:5050",
     case Method of
         post ->
-            {Method, {Host ++ binary_to_list(Path), Headers, "", <<"">>}};
+            {Method, Host ++ binary_to_list(Path), HeadersBinary, <<>>, []};
         _ ->
-            {Method, {Host ++ binary_to_list(Path), Headers}}
+            {Method, Host ++ binary_to_list(Path), HeadersBinary, <<>>, []}
     end.
 
 proxy_request(Req) ->
-    {Method, Request} = prepare_request(Req),
-    httpc:request(Method, Request, [], []).
+    {Method, Url, Headers, Body, Options} = prepare_request(Req),
+    hackney:request(Method, Url, Headers, Body, Options).
 
 handle(Req, State=#state{}) ->
     Resp = proxy_request(Req),
     io:fwrite("~p ~n", [Resp]),
     case Resp of
-        {ok, {{_Version, Status, _Reason}, Headers, Body}} ->
-            BinaryHeaders = [{binary:list_to_bin(H), binary:list_to_bin(V)} ||
-                                {H, V} <- Headers],
+        {ok, StatusCode, RespHeaders, ClientRef} ->
+            %% BinaryHeaders = [{binary:list_to_bin(H), binary:list_to_bin(V)} ||
+            %%                     {H, V} <- Headers],
+            {ok, Body} = hackney:body(ClientRef),
             {ok, Req2} = cowboy_req:reply(
-                           Status,
-                           BinaryHeaders ++ [{<<"X-Proxy">>, <<"yobwoc">>}],
+                           StatusCode,
+                           RespHeaders ++ [{<<"X-Proxy">>, <<"yobwoc">>}],
                            Body,
                            Req),
             {ok, Req2, State};
